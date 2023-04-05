a94bf441b56b6322e0b9d6c54b7ace202f27582e
Ecophagy <olly.spoon@gmail.com>
2023-04-05 21:13:42 +0100

    Add Score and font for scrore

---
+++ PaddleBounceBlockBreak/Score.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PaddleBounceBlockBreak
+{
+    public class Score
+    {
+        public int CurrentScore;
+
+        private SpriteFont _font;
+
+        public Score(SpriteFont font)
+        {
+            _font = font;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // TODO: Change hardcoded position to handle resizing
+            spriteBatch.DrawString(_font, CurrentScore.ToString(), new Vector2(320, 70), Color.White);
+        }
+    }
+}
