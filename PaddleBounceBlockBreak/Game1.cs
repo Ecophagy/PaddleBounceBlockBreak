ba1a84ce175995a28117e294489f79b1b0420993
Ecophagy <olly.spoon@gmail.com>
2025-02-19 19:41:20 +0000

    Move systems up to Game from Level
    This means we:
    - Do not create and destroy systems as we load a level
    - Can do "game" systems with "level" components (e.g. score)
    
    It does feel weird to expose so much of level and effectively remove
    level.update() and level.draw() though...

--- PaddleBounceBlockBreak/Game1.cs
+++ PaddleBounceBlockBreak/Game1.cs
@@ -4,6 +4,7 @@ using Microsoft.Xna.Framework.Input;
 using PaddleBounceBlockBreak.Constants;
 using PaddleBounceBlockBreak.HUD;
 using System;
+using PaddleBounceBlockBreak.Systems;
 
 namespace PaddleBounceBlockBreak
 {
@@ -25,6 +26,15 @@ namespace PaddleBounceBlockBreak
 
         private HudText _gameOverOverlay;
         private DynamicHudText _livesOverlay;
+        
+        // Systems
+        private readonly RenderSystem _renderSystem = new();
+        private readonly UserInputSystem _userInputSystem = new();
+        private readonly MotionSystem _motionSystem = new();
+        private readonly AccelerationSystem _accelerationSystem = new();
+        private readonly EntityCollisionSystem _entityCollisionSystem = new();
+        private readonly WallCollisionSystem _wallCollisionSystem = new();
+        private readonly CollisionDamageSystem _collisionDamageSystem = new();
         
 
         public Game1()
@@ -77,7 +87,51 @@ namespace PaddleBounceBlockBreak
 
             if (_gameState == GameState.GAME_ACTIVE)
             {
-                _level.Update(gameTime);
+                if (_level.LevelState == LevelState.LEVEL_ACTIVE)
+                {
+                    foreach (var (entityId, component) in _level.UserControlComponents)
+                    {
+                        _userInputSystem.Update(component, _level.MotionComponents[entityId]);
+                    }
+
+                    // Handles collision between ball and other objects
+                    foreach (var (entityId, component) in _level.CollisionComponents)
+                    {
+                        if (entityId != _level.BallEntity.EntityId)
+                        {
+                            _entityCollisionSystem.Update(_level.CollisionComponents[_level.BallEntity.EntityId],
+                                _level.PositionComponents[_level.BallEntity.EntityId],
+                                _level.MotionComponents[_level.BallEntity.EntityId],
+                                component,
+                                _level.PositionComponents[entityId]);
+                        }
+                    }
+
+                    // Handle ball collision with walls
+                    _wallCollisionSystem.Update(_level.CollisionComponents[_level.BallEntity.EntityId],
+                        _level.PositionComponents[_level.BallEntity.EntityId],
+                        _level.MotionComponents[_level.BallEntity.EntityId],
+                        Game1.ScreenWidth,
+                        Game1.ScreenHeight);
+
+                    foreach (var (entityId, component) in _level.HealthComponents)
+                    {
+                        _collisionDamageSystem.Update(component, _level.CollisionComponents[entityId],
+                            _level.RenderComponents[entityId]);
+                    }
+
+                    foreach (var (entityId, component) in _level.AccelerationComponents)
+                    {
+                        _accelerationSystem.Update(gameTime, component, _level.MotionComponents[entityId]);
+                    }
+
+                    foreach (var (entityId, component) in _level.MotionComponents)
+                    {
+                        _motionSystem.Update(component, _level.PositionComponents[entityId]);
+                    }
+
+                    PostUpdate();
+                }
 
                 if (_level.LevelState == LevelState.LEVEL_FAIL)
                 {
@@ -102,13 +156,33 @@ namespace PaddleBounceBlockBreak
         }
 
 
+        private void PostUpdate()
+        {
+            foreach (var (entityId, component) in _level.HealthComponents)
+            {
+                if (component.Health <= 0)
+                {
+                    // FIXME: Not very scalable, might be worth having a list of component lists
+                    // That we can iterate over to find elements that need removing
+                    _level.RenderComponents.Remove(entityId);
+                    _level.PositionComponents.Remove(entityId);
+                    _level.CollisionComponents.Remove(entityId);
+                    _level.HealthComponents.Remove(entityId);
+                }
+            }
+        }
+
+
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.LightSlateGray);
 
             _spriteBatch.Begin();
 
-            _level.Draw(gameTime, _spriteBatch);
+            foreach (var (entityId, component) in _level.RenderComponents)
+            {
+                _renderSystem.Draw(_spriteBatch, component, _level.PositionComponents[entityId]);
+            }
 
             _scoreOverlay.Draw(_spriteBatch, (_totalScore + _level.LevelScore).ToString());
             _livesOverlay.Draw(_spriteBatch, _lives.ToString());
--- PaddleBounceBlockBreak/Level.cs
+++ PaddleBounceBlockBreak/Level.cs
@@ -22,26 +22,17 @@ namespace PaddleBounceBlockBreak
         private Paddle _paddle;
         private List<Block> _blocks;
         
-        private readonly Entity _ballEntity = new Entity("ball"); 
-        private readonly Entity _paddleEntity = new Entity("paddle");
+        public readonly Entity BallEntity = new Entity("ball"); 
+        public readonly Entity PaddleEntity = new Entity("paddle");
 
         // Component lists
-        private Dictionary<Guid, RenderComponent> _renderComponents = new();
-        private Dictionary<Guid, PositionComponent> _positionComponents = new();
-        private Dictionary<Guid, MotionComponent> _motionComponents = new();
-        private Dictionary<Guid, AccelerationComponent> _accelerationComponents = new();
-        private Dictionary<Guid, UserControlComponent> _userControlComponents = new();
-        private Dictionary<Guid, CollisionComponent> _collisionComponents = new();
-        private Dictionary<Guid, HealthComponent> _healthComponents = new();
-        
-        // Systems
-        private readonly RenderSystem _renderSystem = new();
-        private readonly UserInputSystem _userInputSystem = new();
-        private readonly MotionSystem _motionSystem = new();
-        private readonly AccelerationSystem _accelerationSystem = new();
-        private readonly EntityCollisionSystem _entityCollisionSystem = new();
-        private readonly WallCollisionSystem _wallCollisionSystem = new();
-        private readonly CollisionDamageSystem _collisionDamageSystem = new();
+        public Dictionary<Guid, RenderComponent> RenderComponents = new();
+        public Dictionary<Guid, PositionComponent> PositionComponents = new();
+        public Dictionary<Guid, MotionComponent> MotionComponents = new();
+        public Dictionary<Guid, AccelerationComponent> AccelerationComponents = new();
+        public Dictionary<Guid, UserControlComponent> UserControlComponents = new();
+        public Dictionary<Guid, CollisionComponent> CollisionComponents = new();
+        public Dictionary<Guid, HealthComponent> HealthComponents = new();
 
         // Level State
         private Random _random = new Random();
@@ -57,23 +48,23 @@ namespace PaddleBounceBlockBreak
            
             // Ball
             var ballTexture = _content.Load<Texture2D>("ball");
-            _renderComponents.Add(_ballEntity.EntityId, new RenderComponent(ballTexture));
-            _positionComponents.Add(_ballEntity.EntityId, new PositionComponent(new Vector2((Game1.ScreenWidth / 2) - (ballTexture.Width / 2), (Game1.ScreenHeight / 2) - (ballTexture.Height / 2))));
-            _motionComponents.Add(_ballEntity.EntityId, new MotionComponent(3f, new Vector2(-3,-3))); // TODO: Set initial velocity to 0
-            _collisionComponents.Add(_ballEntity.EntityId, new CollisionComponent(ballTexture.Height, ballTexture.Width));
-            _accelerationComponents.Add(_ballEntity.EntityId, new AccelerationComponent(new Vector2(1.5f, 1.5f)));
+            RenderComponents.Add(BallEntity.EntityId, new RenderComponent(ballTexture));
+            PositionComponents.Add(BallEntity.EntityId, new PositionComponent(new Vector2((Game1.ScreenWidth / 2) - (ballTexture.Width / 2), (Game1.ScreenHeight / 2) - (ballTexture.Height / 2))));
+            MotionComponents.Add(BallEntity.EntityId, new MotionComponent(3f, new Vector2(-3,-3))); // TODO: Set initial velocity to 0
+            CollisionComponents.Add(BallEntity.EntityId, new CollisionComponent(ballTexture.Height, ballTexture.Width));
+            AccelerationComponents.Add(BallEntity.EntityId, new AccelerationComponent(new Vector2(1.5f, 1.5f)));
             
             // Paddle
             var paddleTexture = _content.Load<Texture2D>("paddle");
-            _renderComponents.Add(_paddleEntity.EntityId, new RenderComponent(paddleTexture));
-            _positionComponents.Add(_paddleEntity.EntityId, new PositionComponent(new Vector2((Game1.ScreenWidth / 2) - (paddleTexture.Width / 2), Game1.ScreenHeight - 40)));
-            _motionComponents.Add(_paddleEntity.EntityId, new MotionComponent(5f, new Vector2()));
-            _userControlComponents.Add(_paddleEntity.EntityId, new UserControlComponent(new Input()
+            RenderComponents.Add(PaddleEntity.EntityId, new RenderComponent(paddleTexture));
+            PositionComponents.Add(PaddleEntity.EntityId, new PositionComponent(new Vector2((Game1.ScreenWidth / 2) - (paddleTexture.Width / 2), Game1.ScreenHeight - 40)));
+            MotionComponents.Add(PaddleEntity.EntityId, new MotionComponent(5f, new Vector2()));
+            UserControlComponents.Add(PaddleEntity.EntityId, new UserControlComponent(new Input()
             {
                 Left = Keys.Left,
                 Right = Keys.Right
             }));
-            _collisionComponents.Add(_paddleEntity.EntityId, new CollisionComponent(paddleTexture.Height, paddleTexture.Width));
+            CollisionComponents.Add(PaddleEntity.EntityId, new CollisionComponent(paddleTexture.Height, paddleTexture.Width));
             
             // Blocks
             var blockTexture = _content.Load<Texture2D>("block");
@@ -88,14 +79,14 @@ namespace PaddleBounceBlockBreak
             foreach (var i in Enumerable.Range(0, 10))
             {
                 var blockEntity = new Entity($"block_{i}");
-                _renderComponents.Add(blockEntity.EntityId, new RenderComponent(blockTexture));
+                RenderComponents.Add(blockEntity.EntityId, new RenderComponent(blockTexture));
                 // Randomise block position
                 var blockX = _random.Next(0, Game1.ScreenWidth - blockTexture.Width);
                 var ylimit = (Game1.ScreenHeight / 2) - blockTexture.Height; // Limit Y to top half of screen
                 var blockY = _random.Next(0, ylimit);
-                _positionComponents.Add(blockEntity.EntityId, new PositionComponent(new Vector2(blockX, blockY)));
-                _collisionComponents.Add(blockEntity.EntityId, new CollisionComponent(blockTexture.Height, blockTexture.Width));
-                _healthComponents.Add(blockEntity.EntityId, new HealthComponent(2, blockTextures));
+                PositionComponents.Add(blockEntity.EntityId, new PositionComponent(new Vector2(blockX, blockY)));
+                CollisionComponents.Add(blockEntity.EntityId, new CollisionComponent(blockTexture.Height, blockTexture.Width));
+                HealthComponents.Add(blockEntity.EntityId, new HealthComponent(2, blockTextures));
             }
             
             LevelState = LevelState.LEVEL_ACTIVE;
@@ -131,51 +122,6 @@ namespace PaddleBounceBlockBreak
 
         public void Update(GameTime gameTime)
         {
-            if (LevelState == LevelState.LEVEL_ACTIVE)
-            {
-                foreach (var (entityId, component) in _userControlComponents)
-                {
-                    _userInputSystem.Update(component, _motionComponents[entityId]);
-                }
-                
-                // Handles collision between ball and other objects
-                foreach (var (entityId, component) in _collisionComponents)
-                {
-                    if (entityId != _ballEntity.EntityId)
-                    {
-                        _entityCollisionSystem.Update(_collisionComponents[_ballEntity.EntityId],
-                            _positionComponents[_ballEntity.EntityId],
-                            _motionComponents[_ballEntity.EntityId],
-                            component,
-                            _positionComponents[entityId]);
-                    }
-                }
-                
-                // Handle ball collision with walls
-                _wallCollisionSystem.Update(_collisionComponents[_ballEntity.EntityId], 
-                    _positionComponents[_ballEntity.EntityId],
-                    _motionComponents[_ballEntity.EntityId], 
-                    Game1.ScreenWidth,
-                    Game1.ScreenHeight);
-
-                foreach (var (entityId, component) in _healthComponents)
-                {
-                    _collisionDamageSystem.Update(component, _collisionComponents[entityId], _renderComponents[entityId]);
-                }
-
-                foreach (var (entityId, component) in _accelerationComponents)
-                {
-                    _accelerationSystem.Update(gameTime, component, _motionComponents[entityId]);
-                }
-                
-                foreach (var (entityId, component) in _motionComponents)
-                {
-                    _motionSystem.Update(component, _positionComponents[entityId]);
-                }
-                
-                PostUpdate();
-            }
-
             /*
             if (LevelState == LevelState.LEVEL_ACTIVE)
             {
@@ -216,21 +162,6 @@ namespace PaddleBounceBlockBreak
             */
         }
 
-        private void PostUpdate()
-        {
-            foreach (var (entityId, component) in _healthComponents)
-            {
-                if (component.Health <= 0)
-                {
-                    // FIXME: Not very scalable, might be worth having a list of component lists
-                    // That we can iterate over to find elements that need removing
-                    _renderComponents.Remove(entityId);
-                    _positionComponents.Remove(entityId);
-                    _collisionComponents.Remove(entityId);
-                    _healthComponents.Remove(entityId);
-                }
-            }
-        }
 
         public void Reset()
         {
@@ -250,18 +181,7 @@ namespace PaddleBounceBlockBreak
 
         public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            foreach (var (entityId, component) in _renderComponents)
-            {
-                _renderSystem.Draw(spriteBatch, component, _positionComponents[entityId]);
-            }
-
-            // _paddle.Draw(spriteBatch);
-            // _ball.Draw(spriteBatch);
-            //
-            // foreach (var block in _blocks)
-            // {
-            //     block.Draw(spriteBatch);
-            // }
+            
         }
 
         /// <summary>
